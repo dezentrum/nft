d7d3df6d2d4eae3fe2df1e693c2099e7b0156f1f
Christoph Bohne <69964846+extrawunsch@users.noreply.github.com>
2023-10-05 17:00:15 +0200

    Fix to Embed Bug

---
+++ MegaPixelArt_SpriteSheet_32px.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="MegaPixelArt_SpriteSheet_32px" tilewidth="32" tileheight="32" tilecount="504" columns="14">
+ <image source="../../../Downloads/MegaPixelArt32x32pxIcons_SpriteSheet/MegaPixelArt32x32pxIcons_SpriteSheet.png" width="448" height="1152"/>
+</tileset>
