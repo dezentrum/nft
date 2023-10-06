b9ffc3cd06a27ddf9fa0399a6c690cbdeee55bd2
Christoph Bohne <69964846+extrawunsch@users.noreply.github.com>
2023-10-06 11:00:33 +0200

    Pushing Mega Sprite

---
+++ Mega Sprite v2.0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mega Sprite v2.0" tilewidth="32" tileheight="32" tilecount="504" columns="14">
+ <image source="tilesets/MegaPixelArt32x32pxIcons_SpriteSheet.png" width="448" height="1152"/>
+</tileset>
